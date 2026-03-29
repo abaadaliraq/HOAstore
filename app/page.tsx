3caa0c0ae77b5caae09e90c1e0df667162a8d5e4
Abaad Iraq <abaadaliraq07@gmail.com>
2026-03-29 17:34:35 +0300

    remove legacy from url

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("/legacy/index.html");
+  return (
+    <iframe
+      src="/legacy/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
 }
